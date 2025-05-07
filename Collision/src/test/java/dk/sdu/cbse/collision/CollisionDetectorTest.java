8d8d39ec9e8cdd43cda2f13a03eae13444598747
MJP <madsjep23@gmail.com>
2025-05-07 10:27:02 +0200

    Added several new tests for CollisionDetection

--- Collision/src/test/java/dk/sdu/cbse/collision/CollisionDetectorTest.java
+++ Collision/src/test/java/dk/sdu/cbse/collision/CollisionDetectorTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CollisionDetectorTest {
     @Test
-    public void canDetectCollision() {
+    public void canDetectNormalCollision() {
         CollisionDetectorSystem collisionDetectorSystem = new CollisionDetectorSystem();
         Entity entity1 = new Entity(0);
         Entity entity2 = new Entity(1);
@@ -67,5 +67,111 @@ public class CollisionDetectorTest {
 
         assertEquals(0, collisionCP1.getCollisions().size());
         assertEquals(0, collisionCP2.getCollisions().size());
+    }
+
+    @Test
+    public void canDetectPointCollision() {
+        CollisionDetectorSystem collisionDetectorSystem = new CollisionDetectorSystem();
+        Entity entity1 = new Entity(0);
+        Entity entity2 = new Entity(1);
+
+        CollisionCP collisionCP1 = new CollisionCP();
+        CollisionCP collisionCP2 = new CollisionCP();
+        entity1.addComponent(collisionCP1);
+        entity2.addComponent(collisionCP2);
+
+        assertEquals(0, collisionCP1.getCollisions().size());
+        assertEquals(0, collisionCP2.getCollisions().size());
+
+        TransformCP transformCP = new TransformCP(0,0,0,0);
+        entity1.addComponent(transformCP);
+        entity2.addComponent(transformCP);
+
+        World world = new World();
+        world.addEntity(entity1);
+        world.addEntity(entity2);
+
+        collisionDetectorSystem.update(null, world);
+
+        assertEquals(1, collisionCP1.getCollisions().size());
+        assertEquals(1, collisionCP2.getCollisions().size());
+
+        assertEquals(entity2, collisionCP1.getCollisions().peek());
+        assertEquals(entity1, collisionCP2.getCollisions().peek());
+    }
+
+    @Test
+    public void canDetectEdgeCollision() {
+        CollisionDetectorSystem collisionDetectorSystem = new CollisionDetectorSystem();
+        Entity entity1 = new Entity(0);
+        Entity entity2 = new Entity(1);
+
+        CollisionCP collisionCP1 = new CollisionCP();
+        CollisionCP collisionCP2 = new CollisionCP();
+        entity1.addComponent(collisionCP1);
+        entity2.addComponent(collisionCP2);
+
+        assertEquals(0, collisionCP1.getCollisions().size());
+        assertEquals(0, collisionCP2.getCollisions().size());
+
+        TransformCP transformCP1 = new TransformCP(0,0,0,1);
+        TransformCP transformCP2 = new TransformCP(0,2,0,1);
+        entity1.addComponent(transformCP1);
+        entity2.addComponent(transformCP2);
+
+        World world = new World();
+        world.addEntity(entity1);
+        world.addEntity(entity2);
+
+        collisionDetectorSystem.update(null, world);
+
+        assertEquals(1, collisionCP1.getCollisions().size());
+        assertEquals(1, collisionCP2.getCollisions().size());
+
+        assertEquals(entity2, collisionCP1.getCollisions().peek());
+        assertEquals(entity1, collisionCP2.getCollisions().peek());
+    }
+
+    @Test
+    public void canDetectMultipleCollisionsOnWithEntity() {
+        CollisionDetectorSystem collisionDetectorSystem = new CollisionDetectorSystem();
+        Entity entity1 = new Entity(0);
+        Entity entity2 = new Entity(1);
+        Entity entity3 = new Entity(2);
+
+        CollisionCP collisionCP1 = new CollisionCP();
+        CollisionCP collisionCP2 = new CollisionCP();
+        CollisionCP collisionCP3 = new CollisionCP();
+        entity1.addComponent(collisionCP1);
+        entity2.addComponent(collisionCP2);
+        entity3.addComponent(collisionCP3);
+
+        assertEquals(0, collisionCP1.getCollisions().size());
+        assertEquals(0, collisionCP2.getCollisions().size());
+        assertEquals(0, collisionCP3.getCollisions().size());
+
+        TransformCP transformCP1 = new TransformCP(0,0,0,1);
+        TransformCP transformCP2 = new TransformCP(0,2,0,1);
+        TransformCP transformCP3 = new TransformCP(0,-2,0,1);
+        entity1.addComponent(transformCP1);
+        entity2.addComponent(transformCP2);
+        entity3.addComponent(transformCP3);
+
+        World world = new World();
+        world.addEntity(entity1);
+        world.addEntity(entity2);
+        world.addEntity(entity3);
+
+        collisionDetectorSystem.update(null, world);
+
+        assertEquals(2, collisionCP1.getCollisions().size());
+        assertEquals(1, collisionCP2.getCollisions().size());
+        assertEquals(1, collisionCP3.getCollisions().size());
+
+        assertTrue(collisionCP1.getCollisions().contains(entity2));
+        assertTrue(collisionCP1.getCollisions().contains(entity3));
+        assertEquals(entity1, collisionCP2.getCollisions().peek());
+        assertEquals(entity1, collisionCP3.getCollisions().peek());
+
     }
 }
